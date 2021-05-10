e77bac732b6cf08672b88fb9e00cd589c76382be
attila5287 <atiturkoz@hotmail.com>
2021-05-10 13:09:14 -0600

    add animate.css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,7 +16,11 @@ function About() {
 			</Animated>
 			;
 			<div class="list-group">
-				<Animated animationIn="bounceInRight" isVisible={true}>
+				<Animated
+					animationIn="bounceInRight"
+					animationInDelay="1000"
+					isVisible={true}
+				>
 					<a class="list-group-item list-group-item-action flex-column align-items-start active">
 						<div class="d-flex w-100 justify-content-between">
 							<h4 class="mb-1">React</h4>
@@ -29,6 +33,7 @@ function About() {
 					animationIn="bounceInLeft"
 					animationOut="fadeOut"
 					isVisible={true}
+					animationInDelay="2000"
 				>
 					<a
 						href="/discover"
@@ -59,48 +64,52 @@ function About() {
 					animationIn="bounceInRight"
 					animationOut="fadeOut"
 					isVisible={true}
+					animationInDelay="3000"
 				>
-
-				<a
-					href="/search"
-					class="list-group-item list-group-item-action flex-column align-items-start"
-				>
-					<div class="d-flex w-100 justify-content-between">
-						<h3 class="mb-1">/Search</h3>
-						<h1 class="fas fa-search mb-0"></h1>
-					</div>
-					<p class="mb-1">
-						This page allows you to search the
-						<a href="https://dog.ceo/dog-api/" class="mx-1">
-							Dog Ceo API
-						</a>
-						for all dogs by breed. After a search is made, a list of all of the
-						results appears underneath.
-					</p>
-				</a>
+					<a
+						href="/search"
+						class="list-group-item list-group-item-action flex-column align-items-start"
+					>
+						<div class="d-flex w-100 justify-content-between">
+							<h3 class="mb-1">/Search</h3>
+							<h1 class="fas fa-search mb-0"></h1>
+						</div>
+						<p class="mb-1">
+							This page allows you to search the
+							<a href="https://dog.ceo/dog-api/" class="mx-1">
+								Dog Ceo API
+							</a>
+							for all dogs by breed. After a search is made, a list of all of
+							the results appears underneath.
+						</p>
+					</a>
 				</Animated>
-				<a
-					href="https://github.com/attila5287/"
-					class="list-group-item list-group-item-action flex-column align-items-start"
-				>
-					<div class="d-flex w-100 justify-content-between">
-						<h6 class="mb-1">@attila5287</h6>
-						<h4 class="fab fa-github mb-0"></h4>
-					</div>
-				</a>
-				<a
-					href="attila5287.github.io/"
-					class="list-group-item list-group-item-action flex-column align-items-start"
-				>
-					<div class="d-flex w-100 justify-content-between">
-						<h6 class="mb-1">Developer</h6>
-						<img
-							src="https://raw.githubusercontent.com/attila5287/img_readme/main/all/dev.jpg"
-							alt="dev-img"
-							class="rounded"
-						/>
-					</div>
-				</a>
+				<Animated animationIn="fadeIn" animationInDelay="4000" isVisible={true}>
+					<a
+						href="https://github.com/attila5287/"
+						class="list-group-item list-group-item-action flex-column align-items-start"
+					>
+						<div class="d-flex w-100 justify-content-between">
+							<h6 class="mb-1">@attila5287</h6>
+							<h4 class="fab fa-github mb-0"></h4>
+						</div>
+					</a>
+				</Animated>
+				<Animated animationIn="fadeIn" animationInDelay="5000" isVisible={true}>
+					<a
+						href="attila5287.github.io/"
+						class="list-group-item list-group-item-action flex-column align-items-start"
+					>
+						<div class="d-flex w-100 justify-content-between">
+							<h6 class="mb-1">Developer</h6>
+							<img
+								src="https://raw.githubusercontent.com/attila5287/img_readme/main/all/dev.jpg"
+								alt="dev-img"
+								class="rounded"
+							/>
+						</div>
+					</a>
+				</Animated>
 			</div>
 		</div>
 	);
--- src/components/pages/Discover.js
+++ src/components/pages/Discover.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import RandomContainer from '../RandomContainer';
-function Discover() {
+import { Animated } from 'react-animated-css';
+
+function Discover () {
 	return (
 		<div>
-			<h1 className="bg-secondary p-0 rounded text-secondary text-center">
-				<i className="fas fa-compass mx-2"></i>
-				<i>Discover</i>
-			</h1>
-
-			<div className="p-1 text-muted mb-5">
-				<h4 className="fas fa-cog fa-pulse mx-0 text-secondary"></h4>
-				<h6 className="fas fa-cog fa-spin mx-0 text-secondary"></h6>
-				<i>Tinder for all dog-persons and dogs out there!</i>
-				<p>
-					<i className="fas fa-paw bg-success p-1 rounded mx-1"></i> Paw up if
-					you like
-					<i className="fas fa-paw bg-primary p-1 rounded fa-flip-vertical mx-1"></i>{' '}
-					Paw down if not
-				</p>
-
-				<RandomContainer />
-			</div>
+			<Animated animationIn="bounceInLeft" isVisible={true}>
+				<h1 className="bg-secondary p-0 rounded text-secondary text-center">
+					<i className="fas fa-compass mx-2"></i>
+					<i>Discover</i>
+				</h1>
+			</Animated>
+			<Animated animationIn="bounceInRight" animationInDelay="1000" isVisible={true}>
+				<div className="p-1 text-muted">
+					<h4 className="fas fa-cog fa-pulse mx-0 text-secondary"></h4>
+					<h6 className="fas fa-cog fa-spin mx-0 text-secondary"></h6>
+					<i>Tinder for all dog-persons and dogs out there!</i>
+					<p className="mb-2">
+						<i className="fas fa-paw bg-success p-1 rounded mx-1"></i> Paw up if
+						you like
+						<i className="fas fa-paw bg-primary p-1 rounded fa-flip-vertical mx-1"></i>{' '}
+						Paw down if not
+					</p>
+				</div>
+			</Animated>
+<Animated animationIn="fadeInUp" animationInDelay="2000" isVisible={ true }>
+			<RandomContainer/>
+	
+</Animated>
 		</div>
 	);
 }
