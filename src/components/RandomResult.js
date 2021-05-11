c77d5acde9785dad15cf9b019d05b4dad7ca23f2
attila5287 <atiturkoz@hotmail.com>
2021-05-11 03:29:02 -0600

    rand img centered

--- src/components/RandomResult.js
+++ src/components/RandomResult.js
@@ -4,7 +4,7 @@ function RandomContainer(props) {
 	return (
 		<div className="mini">
 			{props.results.map((result) => (
-				<div key={result}>
+				<div key={result} className='text-center container-fluid'>
 					<img
 						alt={result}
 						className="img-fluid border-0 bg-secondary rounded shadow-lg mb-2"
